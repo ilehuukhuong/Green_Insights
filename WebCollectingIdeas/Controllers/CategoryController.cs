b2bf1105f65fe90a520d57884f7a9d40cad0335d
Hữu Khương Lê <ilehuukhuong@gmail.com>
2023-02-17 15:08:47 +0700

    Add project files.

---
+++ WebCollectingIdeas/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCollectingIdeas.Data;
+using WebCollectingIdeas.Models;
+
+namespace WebCollectingIdeas.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ WebCollectingIdeas/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCollectingIdeas.Models;
+
+namespace WebCollectingIdeas.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
