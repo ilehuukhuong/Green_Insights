67ded4a76394f00a6910a0e62021f73408a76f54
Hiệp Phạm Văn <kaissken@outlook.com>
2023-02-19 00:49:39 +0700

    BE: - Create CategoryController - Create CRUD Category View - Edit Shared\Layout.cshtml - Create Shared\_Notification.cshtml FE: Add Bootswatch, Bootstrap

--- WebCollectingIdeas/Controllers/CategoryController.cs
+++ WebCollectingIdeas/Controllers/CategoryController.cs
@@ -18,5 +18,92 @@ namespace WebCollectingIdeas.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {           
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Create successfully";
+                return RedirectToAction("index");
+            }
+            return View(obj);
+           
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id ==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryformDb = _db.Categories.Find(id);
+            //var categoryformDbFirst = _db.Categories.FirstOrDefault(u=>u.Id== id);
+            //var categoryformDbsingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryformDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryformDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Edit successfully";
+                return RedirectToAction("index");
+            }
+            return View(obj);
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryformDb = _db.Categories.Find(id);
+            //var categoryformDbFirst = _db.Categories.FirstOrDefault(u=>u.Id== id);
+            //var categoryformDbsingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryformDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryformDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            //var categoryformDbFirst = _db.Categories.FirstOrDefault(u=>u.Id== id);
+            //var categoryformDbsingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Delete successfully";
+                return RedirectToAction("index");
+            }
+                
+            return View(obj);
+
+        }
     }
 }
