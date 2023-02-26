072e6aebc862eeb434996c01882fdf9106dcf6cc
Hữu Khương Lê <ilehuukhuong@gmail.com>
2023-02-26 16:51:03 +0700

    Add Admin and User Area

--- WebCollectingIdeas/Program.cs
+++ WebCollectingIdeas/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:User}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
