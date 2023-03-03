e6da8da9fe01cd84d2064b26cb7e5bb6367b99b9
Anh <anhnpgcs200323@fpt.edu.vn>
2023-03-03 16:37:52 +0700

    FE: Update Navbar

--- WebCollectingIdeas/wwwroot/js/site.js
+++ WebCollectingIdeas/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+let srcBtn = document.querySelector('bx-search-alt');
+
+btn.onclick = function () {
+    sidebar.classList.toggle('active');
+}
+
+srcBtn.onclick = function () {
+    sidebar.classList.toggle('active');
+}
