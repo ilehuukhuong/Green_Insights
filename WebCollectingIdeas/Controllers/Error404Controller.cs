bd1f587040e85b63bf01a456d6fac72da94f0b16
Anh <anhnpgcs200323@fpt.edu.vn>
2023-03-06 00:12:59 +0700

    Update FE: Category, Idea and Layout

---
+++ WebCollectingIdeas/Controllers/Error404Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCollectingIdeas.Controllers
+{
+    public class Error404Controller : Controller
+    {
+        [Route("error/404")]
+        public IActionResult Error404()
+        {
+            return View("Error404");
+        }
+
+    }
+}
